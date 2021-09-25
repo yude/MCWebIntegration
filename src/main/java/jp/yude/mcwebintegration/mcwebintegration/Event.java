84f2a13083b9be011686e0bb32bc068415838c9a
yudejp <i@yude.jp>
2021-09-25 16:05:26 +0900

    Update SQL update / retrieve implementation, Make event runs on login / logout

--- src/main/java/jp/yude/mcwebintegration/mcwebintegration/Event.java
+++ src/main/java/jp/yude/mcwebintegration/mcwebintegration/Event.java
@@ -3,6 +3,7 @@ package jp.yude.mcwebintegration.mcwebintegration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.sql.Connection;
@@ -12,7 +13,7 @@ import java.sql.SQLException;
 
 public class Event implements Listener {
     @EventHandler
-    public void onEvent(PlayerQuitEvent event) {
+    private void onLogin(PlayerLoginEvent event) {
         Player player = event.getPlayer();
         Connection connection = Database.Database();
         // Save player's data to db
@@ -21,15 +22,44 @@ public class Event implements Listener {
             PreparedStatement stmt_search = connection.prepareStatement(sql_search);
             ResultSet results_search = stmt_search.executeQuery();
             if (results_search.next()) {
-                String sql = "UPDATE `timestamp` SET `last` = '" + System.currentTimeMillis() / 1000L + "' WHERE `uuid` = '" + player.getUniqueId() + "';";
+                String sql = "UPDATE `timestamp` SET `last` = ? WHERE `uuid` = ?;";
                 PreparedStatement stmt = connection.prepareStatement(sql);
                 stmt.setLong(1, System.currentTimeMillis() / 1000L);
                 stmt.setString(2, player.getUniqueId().toString());
-
-                stmt.executeUpdate();
             } else {
-                String sql = "INSERT INTO `timestamp` (uuid, last, first) VALUES ('" + player.getUniqueId() + "', '" + System.currentTimeMillis() / 1000L + "', '" + player.getFirstPlayed() / 1000L + "');";
+                String sql = "INSERT INTO `timestamp` (uuid, last, first) VALUES (?, ?, ?);";
                 PreparedStatement stmt = connection.prepareStatement(sql);
+                stmt.setString(1, player.getUniqueId().toString());
+                stmt.setLong(2, System.currentTimeMillis() / 1000L);
+                stmt.setLong(3, player.getFirstPlayed() / 1000L);
+                stmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // In case MCWebIntegration is loaded after server starts
+    @EventHandler
+    private void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        Connection connection = Database.Database();
+        // Save player's data to db
+        String sql_search = "SELECT * FROM `timestamp` WHERE uuid = '" + player.getUniqueId() +"';";
+        try {
+            PreparedStatement stmt_search = connection.prepareStatement(sql_search);
+            ResultSet results_search = stmt_search.executeQuery();
+            if (results_search.next()) {
+                String sql = "UPDATE `timestamp` SET `last` = ? WHERE `uuid` = ?;";
+                PreparedStatement stmt = connection.prepareStatement(sql);
+                stmt.setLong(1, System.currentTimeMillis() / 1000L);
+                stmt.setString(2, player.getUniqueId().toString());
+            } else {
+                String sql = "INSERT INTO `timestamp` (uuid, last, first) VALUES (?, ?, ?);";
+                PreparedStatement stmt = connection.prepareStatement(sql);
+                stmt.setString(1, player.getUniqueId().toString());
+                stmt.setLong(2, System.currentTimeMillis() / 1000L);
+                stmt.setLong(3, player.getFirstPlayed() / 1000L);
                 stmt.executeUpdate();
             }
         } catch (SQLException e) {
